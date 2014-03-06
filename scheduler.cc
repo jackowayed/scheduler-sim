a7820be4916ca7e9f89530c5f6d5f010624ab287
Daniel Jackoway <commits@danieljackoway.com>
2014-03-06 15:59:00 -0800

    SJN scheduler complete

--- scheduler.cc
+++ scheduler.cc
@@ -1,22 +1,19 @@
+#include <algorithm>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-void FifoSchedule(vector<int> & resources, vector<int> & requests, vector<vector<int> > & results) {
-    int resourceIdx = 0;
-    for (int i = 0; i < requests.size(); i++) {
-        results.push_back(vector<int>());
-        for (int res = 0; res < requests[i]; res++) {
-            results[i].push_back(resourceIdx);
-            resourceIdx++;
+
+void PrintSchedule(vector<vector<int> > & schedule) {
+    for (int i = 0; i < schedule.size(); i++) {
+        for (int j = 0; j < schedule[i].size(); j++) {
+            cout << schedule[i][j] << " ";
         }
+        cout << endl;
     }
 }
-
-/*vector<vector<int> > SjnSchedule(vector<int> & resources, vector<int> & requests) {
-    
-  }*/
 
 void PrintStats(vector<vector<int> > & schedule) {
     // avg time to completion.
@@ -28,22 +25,67 @@ void PrintStats(vector<vector<int> > & schedule) {
     cout << "Avg time until scheduling: " << sumCompletionTimes / schedule.size() << endl;
 }
 
-int main() {
-    cout << "Mesosphere challenge\n";
-    int resourceStream[] = {1, 3, 2, 8, 3, 4, 5, 6, 1, 7, 8};
-    vector<int> resources(resourceStream, resourceStream + sizeof(resourceStream) / sizeof(resourceStream[0]));
 
-    int requestStream[] = {1, 1, 5};
-    vector<int> requests(requestStream, requestStream + sizeof(requestStream) / sizeof(requestStream[0]));
-    vector<vector<int> > fifoSched = vector<vector<int> >();
-    FifoSchedule(resources, requests, fifoSched);
-    
-    for (int i = 0; i < fifoSched.size(); i++) {
-        for (int j = 0; j < fifoSched[i].size(); j++) {
-            cout << fifoSched[i][j] << " ";
+void FifoSchedule(vector<int> & resources, vector<int> & requests,
+                  vector<vector<int> > & results) {
+    int resourceIdx = 0;
+    for (int i = 0; i < requests.size(); i++) {
+        results.push_back(vector<int>());
+        for (int res = 0; res < requests[i]; res++) {
+            results[i].push_back(resourceIdx);
+            resourceIdx++;
         }
-        cout << endl;
     }
+}
 
+/* Compare a pair<int,int> based on their first,
+   with second being tiebreaker */
+bool compare(pair<int,int> one, pair<int,int> two) {
+    if (one.first != two.first) {
+        return one.first < two.first;
+    }
+    return one.second < two.second;
+}
+
+void SjnSchedule(vector<int> & resources, vector<int> & requests,
+                 vector<vector<int> > & results) {
+    vector<pair<int, int> > requestsWithIndices;
+    for (int i = 0; i < requests.size(); i++) {
+        requestsWithIndices.push_back(pair<int, int>(requests[i], i));
+    }
+    sort(requestsWithIndices.begin(), requestsWithIndices.end(), compare);
+
+    vector<int> sortedRequests;
+    for (int i = 0; i < requestsWithIndices.size(); i++) {
+        sortedRequests.push_back(requestsWithIndices[i].first);
+    }
+    vector<vector<int> > sortedSchedule;
+    FifoSchedule(resources, sortedRequests, sortedSchedule);
+
+    for (int i = 0 ; i < requestsWithIndices.size(); i++) {
+        results[requestsWithIndices[i].second] = sortedSchedule[i];
+    }
+}
+
+int main() {
+    int resourceStream[] = {1, 3, 2, 8, 3, 4, 5, 6, 1, 7, 8, 2, 1, 7, 9, 6};
+    vector<int> resources(resourceStream, resourceStream 
+                          + sizeof(resourceStream) / sizeof(resourceStream[0]));
+
+    int requestStream[] = {10, 1, 1};
+    vector<int> requests(requestStream, requestStream
+                         + sizeof(requestStream) / sizeof(requestStream[0]));
+
+    vector<vector<int> > fifoSched;
+    FifoSchedule(resources, requests, fifoSched);
+
+    PrintSchedule(fifoSched);
     PrintStats(fifoSched);
+
+    vector<vector<int> > sjnSchedule(requests.size());
+    SjnSchedule(resources, requests, sjnSchedule);
+
+    cout << "----" << endl;
+    PrintSchedule(sjnSchedule);
+    PrintStats(sjnSchedule);
 }
